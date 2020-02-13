5405322d6a95e9e0d3efc613560e712591142b94
Nusreta Sinanovic <nusreta.sinanovic@cegeka.com>
2020-02-13 13:48:38 +0100

    entity compared with null - null reference exception fix

--- Tactical.DDD.Tests/AggregateTests.cs
+++ Tactical.DDD.Tests/AggregateTests.cs
@@ -51,6 +51,29 @@ namespace Tactical.DDD.Tests
         }
 
         [Fact]
+        public void EntityComparedWithNull_ShouldNotBeEqual()
+        {
+            var item0 = BacklogItem.FromSummary(new BacklogItemId(), "item summary");
+            BacklogItem item1 = null;
+
+            Assert.False(item0.Equals(item1));
+            Assert.NotEqual(item0, item1);
+            Assert.False(item0 == item1);
+            Assert.True(item0 != item1);
+        }
+
+        [Fact]
+        public void NullComparedWithEntity_ShouldNotBeEqual()
+        {
+            BacklogItem item0 = null;
+            var item1 = BacklogItem.FromSummary(new BacklogItemId(), "item summary");
+
+            Assert.NotEqual(item0, item1);
+            Assert.False(item0 == item1);
+            Assert.True(item0 != item1);
+        }
+
+        [Fact]
         public void ESAggregateA_Applies_Domain_Events()
         {
             var id = new BacklogItemId();
